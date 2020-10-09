93ba62c34ecedebf4dc6b0f8869e52e6b1e27213
Zero <47561709+Zer077@users.noreply.github.com>
2020-10-09 09:39:13 +0200

    Add files via upload

---
+++ DeviceAdministrator-master/app/src/main/java/net/globel/deviceadministrator/DeviceAdministratorReceiver.java
@@ -0,0 +1,29 @@
+package net.globel.deviceadministrator;
+
+import android.app.admin.DeviceAdminReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+ 
+public class DeviceAdministratorReceiver extends DeviceAdminReceiver {
+
+    private static final String TAG = "DeviceAdminReceiver";
+
+    @Override
+    public void onEnabled(Context context, Intent intent) {
+        super.onEnabled(context, intent);
+        Toast.makeText(context, "Admin Enabled!" , Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG , "Admin Enabled!");
+    }
+
+    @Override
+    public void onDisabled(Context context, Intent intent) {
+        super.onDisabled(context, intent);
+        Toast.makeText(context, "Admin Disabled!" , Toast.LENGTH_SHORT).show();
+
+        Log.d(TAG , "Admin Disabled!");
+    }
+}
